be800c49348304b4ba216b51d4c6f83a18d0bd65
flavien-hugs <geekactux@gmail.com>
2020-06-30 21:59:11 +0000

    add apps cart, dashbord and update design

--- shop/urls.py
+++ shop/urls.py
@@ -6,7 +6,7 @@ app_name = 'shop'
 urlpatterns = [
     # url product
     path('', HomeListView.as_view(), name="home"),
-    path('product/detail/<slug>/', ProductDetailView.as_view(), name="produit_detail"),
+    path('product/detail/<slug>/', ProductDetailView.as_view(),name="produit_detail"),
     # url category
     path('category/', CategoryListView.as_view(), name='all_category_list'),
     path('category/<slug>/', CategoryListView.as_view(), name='detail_category'),
