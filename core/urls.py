042d4d58c94546b8e85881b1df3c39e9e7c62042
flavien-hugs <geekactux@gmail.com>
2020-07-02 16:52:13 +0000

    update all

--- core/urls.py
+++ core/urls.py
@@ -14,7 +14,3 @@ urlpatterns = [
     path('order/', include('orders.urls', namespace='orders')),
     path('admin/', admin.site.urls),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
