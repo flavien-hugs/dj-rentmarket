f89f052c6904f74c7b2640dff7dad004cabd035b
flavien-hugs <geekactux@gmail.com>
2020-07-02 17:03:19 +0000

    update all

--- core/settings.py
+++ core/settings.py
@@ -137,7 +137,7 @@ USE_I18N = USE_L10N = USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
--- core/urls.py
+++ core/urls.py
@@ -13,4 +13,10 @@ urlpatterns = [
     path('location/', include('location.urls', namespace='location')),
     path('order/', include('orders.urls', namespace='orders')),
     path('admin/', admin.site.urls),
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
